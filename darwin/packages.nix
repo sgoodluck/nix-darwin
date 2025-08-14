b6533aa4a7276809a03644ca485f9bda2725b886
Seth <sethgoodluck@pm.me>
2025-08-13 18:28:07 -0700

    Adds logi options

--- darwin/packages.nix
+++ darwin/packages.nix
@@ -5,10 +5,4 @@
   imports = [
     ../modules/common/packages.nix
   ];
-  
-  # Add host-specific brews if defined
-  homebrew.brews = lib.mkAfter (hostConfig.extraBrews or []);
-  
-  # Add host-specific casks if defined
-  homebrew.casks = lib.mkAfter (hostConfig.extraCasks or []);
 }
