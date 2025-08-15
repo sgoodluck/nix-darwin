4d8f9c656d91faed88b8105b055206cdc93a2fdf
Seth <sethgoodluck@pm.me>
2025-08-15 15:36:23 -0700

    fix: personal

--- hosts/personal/default.nix
+++ hosts/personal/default.nix
@@ -7,7 +7,7 @@
   fullName = "Seth";
   email = "sethgoodluck@pm.me";
   githubUsername = "Seth";
-  gpgKey = ""; # Add your GPG key ID here if you have one
+  gpgKey = "E322D2003CDAA1E0"; # Add your GPG key ID here if you have one
   
   # Machine details
   machineName = "sgoodluck-m1air";
@@ -18,11 +18,17 @@
   extraCasks = [
     # Creative tools
     "orcaslicer"    # 3D printing slicer
+    "raspberry-pi-imager"  # Raspberry Pi OS imaging tool
+    "balenaetcher"  # OS image flasher
     
     # Privacy & utilities
     "transmission"  # BitTorrent client
     "protonvpn"     # VPN service
     "proton-pass"   # Password manager
+    "brave-browser" # Privacy-focused web browser
+    
+    # Productivity
+    "obsidian"      # Knowledge management and note-taking
   ];
   
   # Universal preferences (inherited from common)
